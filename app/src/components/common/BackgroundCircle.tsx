e93c391e9e3815fe96a514a0a50469e12548c84e
Oliver <rift95@live.se>
2021-03-24 16:02:18 +0100

    background circle

---
+++ app/src/components/common/BackgroundCircle.tsx
@@ -0,0 +1,43 @@
+import { Theme, makeStyles, createStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import React from 'react';
+
+const circleDiameter = '712px';
+const circleOffsetPercentage = '0.83';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      position: 'absolute',
+      background: theme.palette.primary.main,
+      height: circleDiameter,
+      width: circleDiameter,
+      borderRadius: '50%',
+    },
+    left: {
+      left: `calc(-${circleOffsetPercentage} * ${circleDiameter})`,
+      top: `calc(50% - ${circleDiameter} * 0.5)`,
+    },
+    right: {
+      right: `calc(-${circleOffsetPercentage} * ${circleDiameter})`,
+      top: `calc(50% - ${circleDiameter} * 0.5)`,
+    },
+    top: {
+      top: `calc(-${circleOffsetPercentage} * ${circleDiameter})`,
+      right: `calc(50% - ${circleDiameter} * 0.5)`,
+    },
+    bottom: {
+      bottom: `calc(-${circleOffsetPercentage} * ${circleDiameter})`,
+      right: `calc(50% - ${circleDiameter} * 0.5)`,
+    },
+  })
+);
+
+interface Props {
+  side: 'left' | 'right' | 'top' | 'bottom';
+}
+
+export const BackgroundCircle = (props: Props) => {
+  const classes = useStyles();
+  return <div className={clsx(classes.root, classes[props.side])}></div>;
+};
--- app/src/routes/SettingsRoute.tsx
+++ app/src/routes/SettingsRoute.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { BackgroundCircle } from '../components/common/BackgroundCircle';
 import { SettingsView } from '../views/SettingsView ';
 
 export const SettingsRoute = () => {
-  return <SettingsView />;
+  return (
+    <>
+      <BackgroundCircle side="left" />
+      <SettingsView />
+    </>
+  );
 };
