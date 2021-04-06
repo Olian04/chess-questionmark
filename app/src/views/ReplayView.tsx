4f672734eb68d1a8deda622985a10be0fdebc6b4
Teremiare <awiker@kth.se>
2021-04-06 11:41:17 +0200

    Playable replays

--- app/src/views/ReplayView.tsx
+++ app/src/views/ReplayView.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Grid, Slider } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Grid, Slider, Button } from '@material-ui/core';
 import {
   Theme,
   makeStyles,
@@ -9,6 +9,7 @@ import {
 import { PlayArrow, SkipNext, SkipPrevious } from '@material-ui/icons';
 
 import { PlayerBar } from '../components/game/PlayerBar';
+import { ReplayBoard } from '../components/game/ReplayBoard';
 import { GameBoard } from '../components/game/GameBoard';
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -43,20 +44,59 @@ const ReplaySlider = withStyles({
 
 export const ReplayView = () => {
   const classes = useStyles();
+  const [turn, setTurn] = useState(1);
+  const [intervalID, setIntervalID] = useState(0);
+  const [playing, setPlaying] = useState(false);
+  const hist = [
+    "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+    "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1",
+    "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2",
+    "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"
+  ];
+
+  const play = () => {
+    let t = turn;
+    setIntervalID(setInterval(() => {
+      if (t >= hist.length) return;
+      setTurn(++t);
+      console.log(t);
+    }, 2000));
+    setPlaying(true);
+  };
+
+  const pause = () => {
+    clearInterval(intervalID);
+    setPlaying(false);
+  };
+
+  const playPause = () => {
+    if (playing) {
+      pause();
+    } else {
+      play();
+    }
+  };
+
   return (
     <Grid container justify="space-between" className={classes.container}>
       <Grid item xs={12}>
         <PlayerBar name="Player 1" rating="1900" icon="/assets/cat.jpg" />
       </Grid>
       <Grid item xs={12}>
-        <GameBoard position="start" />
+        <GameBoard position={hist[turn-1]} transitionDuration={400} draggable={false} />
       </Grid>
       <Grid item container xs={12} justify="center">
-        <SkipPrevious fontSize="large" color="action" />
-        <PlayArrow fontSize="large" color="action" />
-        <SkipNext fontSize="large" color="action" />
+        <Button disabled={turn <= 1} variant="outlined" onClick={() => setTurn(turn - 1)}>
+          <SkipPrevious fontSize="large" color="action" />
+        </Button>
+        <Button variant="outlined" onClick={() => playPause()}>
+          <PlayArrow fontSize="large" color="action" />
+        </Button>
+        <Button disabled={turn >= hist.length} variant="outlined" onClick={() => setTurn(turn + 1)}>
+          <SkipNext fontSize="large" color="action" />
+        </Button>
       </Grid>
-      <ReplaySlider marks={true} min={1} max={20} />
+      <ReplaySlider marks={true} min={1} max={hist.length} value={turn} onChange={(e, v) => setTurn(v)} />
       <Grid item xs={12}>
         <PlayerBar name="Player 2" rating="2000" icon="/assets/cat.jpg" />
       </Grid>
