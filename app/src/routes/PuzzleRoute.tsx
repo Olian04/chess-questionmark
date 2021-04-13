1fea3d0c72e7d36365b10b51c156ee744b8ff343
landeholt <johnlan@kth.se>
2021-04-13 16:30:37 +0200

    added changes to puzzle and replay routes

--- app/src/routes/PuzzleRoute.tsx
+++ app/src/routes/PuzzleRoute.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSetRecoilState } from 'recoil';
 import { PuzzlePresenter } from '../presenters/PuzzlePresenter';
+import { backgroundCircleState } from '../state/backgroundCircle';
+import { pillState } from '../state/pill';
 
 export const PuzzleRoute = () => {
+  const setSide = useSetRecoilState(backgroundCircleState);
+  const setPill = useSetRecoilState(pillState);
+  useEffect(() => {
+    setSide('hidden');
+    setPill(0);
+  });
   return (
     <>
       <PuzzlePresenter />
--- app/src/routes/ReplayRoute.tsx
+++ app/src/routes/ReplayRoute.tsx
@@ -1,12 +1,16 @@
 import React, { useEffect } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { backgroundCircleState } from '../state/backgroundCircle';
+import { pillState } from '../state/pill';
 import { ReplayView } from '../views/ReplayView';
 
 export const ReplayRoute = () => {
   const setSide = useSetRecoilState(backgroundCircleState);
-
-  useEffect(() => setSide('hidden'));
+  const setPill = useSetRecoilState(pillState);
+  useEffect(() => {
+    setSide('hidden');
+    setPill(1);
+  });
   return (
     <>
       <ReplayView />
